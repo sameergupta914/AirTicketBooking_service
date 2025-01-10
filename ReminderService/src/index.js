2a7c09e91e37f889393d6859d1340e21a7fc0531
Sameer Gupta <sameerguptaa09@gmail.com>
2025-01-11 02:05:12 +0530

    added notification cron in remider service

--- ReminderService/src/index.js
+++ ReminderService/src/index.js
@@ -2,22 +2,25 @@ const express=require('express');
 const bodyParser=require('body-parser');
 const { PORT } = require('./config/serverConfig');
 const { sendBasicEmail } = require('./service/email-service');
+const TicketController=require('./controllers/ticket-controller');
+const jobs=require('./utils/jobs');
 
 const setupAndStartServer = () =>{
     const app=express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
 
-    // app.use('/api', apiRoutes);
-
+    app.post('/api/v1/tickets', TicketController.create);
+    
     app.listen(PORT, async () => {
         console.log(`Server Started on Port: ${PORT}`);
-        sendBasicEmail(
-            // 'support@admin.com',
-            'samgupta9142@gmail.com',
-            'This is a test email',
-            'whats the issue we want to know the reason so that we can improve in the further interactions'
-        );
+        jobs();
+        // sendBasicEmail(
+        //     // 'support@admin.com',
+        //     'samgupta9142@gmail.com',
+        //     'This is a test email',
+        //     'whats the issue we want to know the reason so that we can improve in the further interactions'
+        // );
     });
 }
 setupAndStartServer();
--- ReminderService/src/service/email-service.js
+++ ReminderService/src/service/email-service.js
@@ -1,9 +1,12 @@
 const sender=require('../config/emailConfig');
+const TicketRepository=require('../repository/ticket-repository');
 
-const sendBasicEmail = async( mailTo, mailSubject, mailBody)=>{
+const repo= new TicketRepository();
+
+const sendBasicEmail = async( mailFrom, mailTo, mailSubject, mailBody)=>{
     try {
         const response= await sender.sendMail({
-            // from: mailFrom,
+            from: mailFrom,
             to: mailTo,
             subject: mailSubject,
             text: mailBody
@@ -12,8 +15,35 @@ const sendBasicEmail = async( mailTo, mailSubject, mailBody)=>{
     } catch (error) {
         console.log(error);
     }
-  
+}
+
+const fetchPendingEmails= async(timestamp)=>{
+    try {
+        const response=await repo.get({status:"PENDING"});
+        return response;
+    } catch (error) {
+        console.log(error);
+    }
+}
+const createNotification= async(data)=>{
+    try {
+        const response=await repo.create(data);
+        return response;
+    } catch (error) {
+        console.log(error);
+    }
+}
+const updateTicket= async(ticketId, data)=>{
+    try {
+        const response=await repo.update(ticketId, data);
+        return response;
+    } catch (error) {
+        console.log(error);
+    }
 }
 module.exports={
-    sendBasicEmail
+    sendBasicEmail,
+    fetchPendingEmails,
+    createNotification,
+    updateTicket
 }
