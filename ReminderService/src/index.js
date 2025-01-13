80532d421d1d928d757a29c20913c9b28fa43868
Sameer Gupta <sameerguptaa09@gmail.com>
2025-01-13 22:37:09 +0530

    added rabbit mq setup

--- ReminderService/src/index.js
+++ ReminderService/src/index.js
@@ -1,20 +1,26 @@
 const express=require('express');
 const bodyParser=require('body-parser');
 const { PORT } = require('./config/serverConfig');
-const { sendBasicEmail } = require('./service/email-service');
+const { createChannel }=require('./utils/messageQueue');
+
+// const { sendBasicEmail } = require('./service/email-service');
+
 const TicketController=require('./controllers/ticket-controller');
 const jobs=require('./utils/jobs');
 
-const setupAndStartServer = () =>{
+const setupAndStartServer = async() =>{
     const app=express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
+
+    // const channel= await createChannel();
 
     app.post('/api/v1/tickets', TicketController.create);
     
     app.listen(PORT, async () => {
         console.log(`Server Started on Port: ${PORT}`);
-        jobs();
+        // jobs();
+
         // sendBasicEmail(
         //     // 'support@admin.com',
         //     'samgupta9142@gmail.com',
