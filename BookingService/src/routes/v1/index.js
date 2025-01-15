b73c15311fe3ab33654534bfde4480e181add9dc
Sameer Gupta <sameerguptaa09@gmail.com>
2025-01-16 02:20:39 +0530

    added api gateway

--- BookingService/src/routes/v1/index.js
+++ BookingService/src/routes/v1/index.js
@@ -6,7 +6,9 @@ const { createChannel }=require('../../utils/messageQueue');
 const bookingController= new BookingController();
 const router=express.Router();
 
-
+router.get('/info', (req,res)=>{
+    return res.json({ message:'response from routes.' });
+})
 router.post('/bookings', bookingController.create);
 router.patch('/bookings/:id', bookingController.update);
 router.post('/publish', bookingController.sendMessageToQueue);
--- FlightAndSearchService/src/index.js
+++ FlightAndSearchService/src/index.js
@@ -10,8 +10,13 @@ const setupAndStartServer=async ()=>{
     const app=express();
     app.use(bodyParser.urlencoded({extended:true}));
 	app.use(bodyParser.json());
+
+    // app.get('/api/v1/home', (req, res)=>{
+    //     return res.json({ message: 'hitting the flight and search service from api gateway' });
+    // })
     app.use('/api', ApiRoutes);
 
+    
 
     app.listen(PORT, async()=>{
         console.log(`server started at ${PORT}`);
